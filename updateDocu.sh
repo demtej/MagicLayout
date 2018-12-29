85b4b1db20d5f07645dc4eb72f9ed782b58b6c5e
Demian Tejo <demian.tejo@gmail.com>
2018-12-28 21:56:00 -0300

    Add Jazzy Script

---
+++ updateDocu.sh
@@ -0,0 +1,9 @@
+jazzy --podspec MagicLayout.podspec \
+  --clean \
+  --author Demian Tejo \
+  --author_url https://demtej.github.io/resume/ \
+  --github_url https://github.com/demtej/MagicLayout \
+  --github-file-prefix https://github.com/demtej/MagicLayout/tree/master \
+  --module-version 4.0.0 \
+  --root-url https://github.com/demtej/MagicLayout \
+  --output documentation/MagicLayout \
